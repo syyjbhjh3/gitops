b0df9d866568f58ea3bda13e5e6c478bb2061518
yj.park <yj.park@okestro.com>
2023-12-13 21:40:48 +0900

    gitops init

--- scripts/gitops-manifest-script.sh
+++ scripts/gitops-manifest-script.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 MODULES=($TARGETS)
 APP_LIST="backend frontend"
-for TARGET in "${MODULES[@]}"
-do
-    echo "Deploy target application: "$TARGET
-    echo "TAG: "$TAG
-
-    helm template -n test $TARGET ./charts/$TARGET -f ./charts/$TARGET/$TARGET.yaml \
-    --set image.tag=${TAG} > ./manifests/cmp-portal/$ENV/$TARGET.yaml
-done
+#for TARGET in "${MODULES[@]}"
+#do
+#    echo "Deploy target application: "$TARGET
+#    echo "TAG: "$TAG
+#
+#    helm template -n test $TARGET ./charts/$TARGET -f ./charts/$TARGET/$TARGET.yaml \
+#    --set image.tag=${TAG} > ./manifests/cmp-portal/$ENV/$TARGET.yaml
+#done
 
 # git add/commit/push
 git add .
