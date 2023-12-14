d41be155fdfd168eed461aa21c23d0a29db132bd
yj.park <yj.park@okestro.com>
2023-12-14 13:47:08 +0900

    gitops Update

--- scripts/gitops-manifest-script.sh
+++ scripts/gitops-manifest-script.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 MODULES=$TARGET
 APP_LIST="backend frontend"
-#for TARGET in "${MODULES[@]}"
-#do
-#    echo "Deploy target application: "$TARGET
-#    echo "TAG: "$TAG
-#
-#    helm template -n test $TARGET ./charts/$TARGET -f ./charts/$TARGET/$TARGET.yaml \
-#    --set image.tag=${TAG} > ./manifests/cmp-portal/$ENV/$TARGET.yaml
-#done
+
+echo "Deploy target application: "$TARGET
+echo "TAG: "$TAG
+
+helm template -n test $TARGET ./charts/$TARGET -f ./charts/$TARGET/$TARGET.yaml \
+     --set image.tag=${TAG} > ./manifests/$TARGET.yaml
 
 # git add/commit/push
 git add .
