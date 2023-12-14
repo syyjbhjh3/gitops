a99af1b10d14b0e51c1951ce555b4f50a08603c6
yj.park <yj.park@okestro.com>
2023-12-14 16:21:29 +0900

    gitops Update

--- scripts/gitops-manifest-script.sh
+++ scripts/gitops-manifest-script.sh
@@ -8,6 +8,7 @@ echo "TAG: "$TAG
 helm template -n test $TARGET charts/$TARGET -f charts/$TARGET/values.yaml \
      --set image.tag=${TAG} > manifests/$TARGET.yaml
 
+git config --global --add safe.directory /var/lib/jenkins/workspace/gitops-manifest-update
 # git add/commit/push
 git add .
 git commit -m "updating manifest file completed."
